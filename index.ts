a3ed711afcb2726c6dbc2dc17199e8a20a9e1a94
k8w <me@k8w.io>
2021-03-26 11:53:02 +0800

    1.1.7 add Logger

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-import { BaseServiceType } from './src/BaseServiceType';
-import { ServiceProto, ServiceDef, ApiServiceDef, MsgServiceDef } from './src/ServiceProto';
-import { ServerInputData, ServerOutputData, ApiError } from './src/TransportData';
-import { TransportDataProto } from './src/TransportDataProto';
-import { TsrpcError } from './src/TsrpcError';
-
-export { BaseServiceType, ServiceProto, ServiceDef, ApiServiceDef, MsgServiceDef, ServerInputData, ServerOutputData, ApiError, TransportDataProto, TsrpcError };
+export { BaseServiceType } from './src/BaseServiceType';
+export { ServiceProto, ServiceDef, ApiServiceDef, MsgServiceDef } from './src/ServiceProto';
+export { ServerInputData, ServerOutputData, ApiError } from './src/TransportData';
+export { TransportDataProto } from './src/TransportDataProto';
+export { TsrpcError } from './src/TsrpcError';
+export { Logger } from './src/Logger';
