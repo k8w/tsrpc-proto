b8053ef7e35504b7934e6349681dcb4b59fe2c8f
k8w <me@k8w.io>
2021-05-21 20:02:16 +0800

    1.3.3-dev.1 export int uint Overwrite ...

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export * from './models/ServiceProto';
 export * from './models/TransportData';
 export * from './models/TransportDataProto';
 export * from './models/TsrpcError';
-
+export { Overwrite, int, uint, bigint64, biguint64 } from 'tsbuffer-schema';
